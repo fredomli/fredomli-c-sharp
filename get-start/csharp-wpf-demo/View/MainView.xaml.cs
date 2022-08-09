b0119c323529ffd92ff8c63ba5fbee977a3c3e69
lishuang <fredomli@163.com>
2022-08-09 16:13:55 +0800

    Windows Presentation Foundation (WPF) project initialize.

--- get-start/csharp-wpf-demo/View/MainView.xaml.cs
+++ get-start/csharp-wpf-demo/View/MainView.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace csharp_wpf_demo
+namespace csharp_wpf_demo.View
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainView : Window
     {
-        public MainWindow()
+        public MainView()
         {
             InitializeComponent();
         }
