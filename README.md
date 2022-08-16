93417cf34ca7dfd801d88af43ca15e5d93891b0a
lishuang <fredomli@163.com>
2022-08-16 12:53:18 +0800

    Add Readme.md file and add  dependency source（Prism and MaterialDesign）

---
+++ README.md
@@ -0,0 +1,15 @@
+# WPF项目实例
+
+## 准备
+使用WPF搭建项目，项目采用`Prism`框架和`Material Design`样式设计。
+
+*依赖：*  
+> Prism Version v7.2.0.1422  
+> MaterialDesign Version v4.5.0
+
+## 创建项目
+使用Visual Studio 2015 创建WPF应用程序。并将创建好的项目使用Git进行
+管理，远程仓库的地址为：`https://github.com/fredomli/fredomli-c-sharp.git`
+
+
+
--- get-start/csharp-wpf-demo/App.xaml.cs
+++ get-start/csharp-wpf-demo/App.xaml.cs
@@ -1,17 +1,28 @@
-﻿using System;
+﻿using Prism.DryIoc;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Prism.Ioc;
 
 namespace csharp_wpf_demo
 {
     /// <summary>
     /// App.xaml 的交互逻辑
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<View.MainView>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
     }
 }
